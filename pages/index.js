d3d620f7eee7dd09bc9ba0841f0b9fbf8db3921a
AugustoFlo <viniau1214@gmail.com>
2024-12-18 18:33:33 +0000

    reformula a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amor se você me ama, diz que me ama mais que sorvete no zap</h1>
+    return <h1>Amoremio se você me ama, diz que me ama mais que sorvete no zap</h1>
 }
 
 export default Home
